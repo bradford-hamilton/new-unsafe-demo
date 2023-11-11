3d34b61773592a4d08d9d9428b4ff85a8cf9924c
bradford lamson-scribner <brad.lamson@gmail.com>
2023-11-11 16:22:26 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type user struct {
+	name    string
+	age     int
+	animals []string
+}
+
+func main() {
+	var u user
+	p(u) // {name: age:0 animals:[]}
+
+	uNamePtr := (*string)(unsafe.Pointer(&u))
+	*uNamePtr = "bradford"
+	p(u) // {name:bradford age:0 animals:[]}
+
+	age := (*int)(unsafe.Add(unsafe.Pointer(&u), unsafe.Offsetof(u.age)))
+	*age = 34
+	p(u) // {name:bradford age:34 animals:[]}
+
+	u.animals = []string{"missy", "ellie", "toby"}
+	secondAnimal := (*string)(unsafe.Add(
+		unsafe.Pointer(unsafe.SliceData(u.animals)), unsafe.Sizeof("")),
+	)
+	p(u) // {name:bradford age:34 animals:[missy ellie toby]}
+
+	*secondAnimal = "carlos"
+	p(u) // {name:bradford age:34 animals:[missy carlos toby]}
+}
+
+func p(a any) { fmt.Printf("%+v\n", a) }
