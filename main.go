3756a23f3ed8921988b4f4113dd369b1976951fe
bradford lamson-scribner <brad.lamson@gmail.com>
2023-11-11 17:09:06 -0700

    feat: add using pointer arithmetic to loop over slice

--- main.go
+++ main.go
@@ -32,6 +32,15 @@ func main() {
 
 	*secondAnimal = "carlos"
 	p(u) // {name:bradford age:34 animals:[missy carlos toby]}
+
+	// ------------------------------------------------
+
+	mySlice := []string{"apples", "oranges", "bananas", "kansas"}
+	start := unsafe.Pointer(unsafe.SliceData(mySlice))
+	size := unsafe.Sizeof(mySlice[0])
+	for i := 0; i < len(mySlice); i++ {
+		p(*(*string)(unsafe.Add(start, uintptr(i)*size)))
+	}
 }
 
 func p(a any) { fmt.Printf("%+v\n", a) }
