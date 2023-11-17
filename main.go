55d213832eb498a7b747d712161f1c82ab617bc1
bradford lamson-scribner <brad.lamson@gmail.com>
2023-11-17 14:46:09 -0700

    fix: fix examples with extra/unnecessary unsafe.Pointer calls, as unsafe.Add returns an unsafe.Pointer

--- main.go
+++ main.go
@@ -23,15 +23,13 @@ func main() {
 	*uNamePtr = "bradford"
 	p(u) // {name:bradford age:0 animals:[]}
 
-	age := (*int)(unsafe.Pointer(
-		unsafe.Add(unsafe.Pointer(&u), unsafe.Offsetof(u.age)),
-	))
+	age := (*int)(unsafe.Add(unsafe.Pointer(&u), unsafe.Offsetof(u.age)))
 	*age = 34
 	p(u) // {name:bradford age:34 animals:[]}
 
 	u.animals = []string{"missy", "ellie", "toby"}
-	secondAnimal := (*string)(unsafe.Pointer(
-		unsafe.Add(unsafe.Pointer(unsafe.SliceData(u.animals)), unsafe.Sizeof("")),
+	secondAnimal := (*string)(unsafe.Add(
+		unsafe.Pointer(unsafe.SliceData(u.animals)), unsafe.Sizeof(""),
 	))
 	p(u) // {name:bradford age:34 animals:[missy ellie toby]}
 
@@ -45,7 +43,7 @@ func main() {
 	size := unsafe.Sizeof(fruits[0])
 
 	for i := 0; i < len(fruits); i++ {
-		p(*(*string)(unsafe.Pointer(unsafe.Add(start, uintptr(i)*size))))
+		p(*(*string)(unsafe.Add(start, uintptr(i)*size)))
 	}
 	// apples
 	// oranges
@@ -60,20 +58,20 @@ func main() {
 	*foo = "bradford"
 	p(ps) // {foo:bradford bar:1337 baz:[100 150 200 250]}
 
-	bar := (*int)(unsafe.Pointer(unsafe.Add(unsafe.Pointer(&ps), unsafe.Sizeof(""))))
+	bar := (*int)(unsafe.Add(unsafe.Pointer(&ps), unsafe.Sizeof("")))
 	*bar = 20
 	p(ps) // {foo:bradford bar:20 baz:[100 150 200 250]}
 
-	slcPtr := (*[]int)(unsafe.Pointer(unsafe.Add(
+	slcPtr := (*[]int)(unsafe.Add(
 		unsafe.Pointer(&ps), (unsafe.Sizeof("") + unsafe.Sizeof(int(0))),
-	)))
+	))
 	p(*slcPtr) // [100 150 200 250]
 
 	start = unsafe.Pointer(unsafe.SliceData(*slcPtr))
 	size = unsafe.Sizeof(int(0))
 
 	for i := 0; i < len(*slcPtr); i++ {
-		p(*(*int)(unsafe.Pointer(unsafe.Add(start, uintptr(i)*size))))
+		p(*(*int)(unsafe.Add(start, uintptr(i)*size)))
 	}
 	// 100
 	// 150
